33efdd1500984d997edecbfe4d9cc84e2737e6e9
Herman Junge <chpdg42@gmail.com>
2016-03-31 05:10:46 +0000

    Cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -18,40 +18,5 @@ gulp.task('build', function(){
     .pipe(gulp.dest(path.DEST_BUILD));
 });
 
-/*
-gulp.task('transform', function(){
-  gulp.src(path.JS)
-    .pipe(react())
-    .pipe(gulp.dest(path.DEST_SRC));
-});
-
-gulp.task('copy', function(){
-  gulp.src(path.HTML)
-    .pipe(gulp.dest(path.DEST));
-});
-
-gulp.task('watch', function(){
-  gulp.watch(path.ALL, ['transform', 'copy']);
-});
-
-gulp.task('build', function(){
-  gulp.src(path.JS)
-    .pipe(react())
-    .pipe(concat(path.MINIFIED_OUT))
-    .pipe(uglify(path.MINIFIED_OUT))
-    .pipe(gulp.dest(path.DEST_BUILD));
-});
-
-gulp.task('replaceHTML', function(){
-  gulp.src(path.HTML)
-    .pipe(htmlreplace({
-      'js': 'build/' + path.MINIFIED_OUT
-    }))
-    .pipe(gulp.dest(path.DEST));
-});
-
-gulp.task('default', ['watch']);
-
-gulp.task('production', ['replaceHTML', 'build']);
-*/
-
+// No olvidar este despues!
+// .pipe(uglify(path.MINIFIED_OUT))
